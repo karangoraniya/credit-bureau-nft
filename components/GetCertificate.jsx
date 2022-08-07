ab8d35799825eafcfd64f7949024eac8c7ab7212
karangorania <karangoraniya@gmail.com>
2022-08-07 16:51:13 +0530

    zora added 🚀

--- components/GetCertificate.jsx
+++ components/GetCertificate.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   Text,
   Row,
@@ -12,17 +12,17 @@ import {
   Image,
   Spacer,
   Link,
-} from "@nextui-org/react";
-import { ethers } from "ethers";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import CreditCertificate from "../artifacts/contracts/CreditCertificate.sol/CreditCertificate.json";
-import Swal from "sweetalert2";
-import { useRouter } from "next/router";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-
+} from '@nextui-org/react';
+import { ethers } from 'ethers';
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import CreditCertificate from '../artifacts/contracts/CreditCertificate.sol/CreditCertificate.json';
+import Swal from 'sweetalert2';
+import { useRouter } from 'next/router';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { Strategies, Networks } from '@zoralabs/nft-hooks';
 
 export const GetCertificate = () => {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const router = useRouter();
   const [maticAmount, setMaticAmount] = useState();
   const [visible, setVisible] = useState(false);
@@ -83,35 +83,51 @@ export const GetCertificate = () => {
       setIsLoading(false);
       closeHandler();
       Swal.fire({
-        icon: "success",
+        icon: 'success',
         title: "MATIC's unlock successfully",
-        html: `<a href="https://rinkeby.etherscan.io/tx/${tx.hash}">View on explorer</a>`,
+        html: `<a href="https://mumbai.polygonscan.com/tx/${tx.hash}">View on explorer</a>`,
       }).then(router.push('/'));
       setIsLoading(false);
     } catch (err) {
       setIsLoading(false);
       console.log(err.message);
       Swal.fire({
-        icon: "error",
-        title: "Something Went Wrong",
+        icon: 'error',
+        title: 'Something Went Wrong',
       });
     }
   };
 
+  // Zora NFT Fetch
+
+  const zora = async () => {
+    const fetchAgent = new Strategies.ZDKFetchStrategy(Networks.MUMBAI);
+
+    // Get result from the server
+    const result = await fetchAgent.fetchNFT(
+      '0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63',
+      '1'
+    );
+    console.log(result);
+  };
+
+  console.log(zora);
+
   const createCertificate = async () => {
     setIsLoading(true);
     const fullName = name;
-    const amount = ethers.utils.parseUnits(maticAmount, "ether");
+    const amount = ethers.utils.parseUnits(maticAmount, 'ether');
     const description = `This is certificate issued for ${fullName} for stacking ${maticAmount} MATIC in credit bureau`;
-    const tokenUri = "https://ipfs.io/ipfs/bafkreieokh3pwl6h2ydnjv3a2cl7aqxed5gfxp6mto74i4p53xminikfyu";
+    const tokenUri =
+      'https://ipfs.io/ipfs/bafkreieokh3pwl6h2ydnjv3a2cl7aqxed5gfxp6mto74i4p53xminikfyu';
     if (maticAmount < 100) {
       setIsLoading(false);
-      alert("Amount should be greater than 100");
+      alert('Amount should be greater than 100');
       return;
     }
     if (!fullName || !maticAmount) {
       setIsLoading(false);
-      alert("Please fill all the input values");
+      alert('Please fill all the input values');
       return;
     }
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -137,25 +153,25 @@ export const GetCertificate = () => {
       setIsLoading(false);
       closeHandler();
       Swal.fire({
-        icon: "success",
-        title: "Certificate generate successfully",
-        html: `<a href="https://rinkeby.etherscan.io/tx/${tx.hash}">View on explorer</a>`,
+        icon: 'success',
+        title: 'Certificate generate successfully',
+        html: `<a href="https://mumbai.polygonscan.com/tx/${tx.hash}">View on explorer</a>`,
       }).then(router.push('/'));
     } catch (err) {
       console.log(err.message);
       setIsLoading(false);
       closeHandler();
       Swal.fire({
-        icon: "error",
-        title: "Something Went Wrong",
+        icon: 'error',
+        title: 'Something Went Wrong',
       });
     }
   };
 
   return (
     <>
-      {nftData && nftData.name !== "" ? (
-        <Row justify="center" align="center" css={{ m: "40px 0 0 0" }}>
+      {nftData && nftData.name !== '' ? (
+        <Row justify="center" align="center" css={{ m: '40px 0 0 0' }}>
           <Grid.Container justify="center" align="center">
             <Grid xs={12} lg={4} md={6} justify="center" align="center">
               <Card>
@@ -168,10 +184,10 @@ export const GetCertificate = () => {
                     </Col>
                   </Row>
                 </Card.Header>
-                <Card.Body css={{ py: "$2" }}>
+                <Card.Body css={{ py: '$2' }}>
                   <Row justify="flex-end">
                     <Col span={12}>
-                      <Text color={"secondary"} justify="center" align="center">
+                      <Text color={'secondary'} justify="center" align="center">
                         Certificate of {nftData.name}
                       </Text>
                     </Col>
@@ -184,7 +200,7 @@ export const GetCertificate = () => {
                     </Col>
                   </Row>
                   <Image
-                    css={{ m: "10px 0 0 0" }}
+                    css={{ m: '10px 0 0 0' }}
                     showSkeleton
                     width={500}
                     height={300}
@@ -197,7 +213,7 @@ export const GetCertificate = () => {
                   <Row
                     justify="center"
                     align="center"
-                    css={{ m: "40px 0 0 0" }}
+                    css={{ m: '40px 0 0 0' }}
                   >
                     {!loading ? (
                       <Button onClick={unLockFunds}>UnLock Funds</Button>
@@ -212,32 +228,33 @@ export const GetCertificate = () => {
         </Row>
       ) : (
         <>
-          <Row justify="center" align="center" css={{ m: "40px 0 0 0" }}>
+          <Row justify="center" align="center" css={{ m: '40px 0 0 0' }}>
             <Text>
-              Lock MATIC to get certificate {"(Only one certificate allowed)"}
+              Lock MATIC to get certificate {'(Only one certificate allowed)'}
             </Text>
           </Row>
-          <Row justify="center" align="center" css={{ m: "10px 0 0 0" }}>
-            {isConnected ? (
+          <Row justify="center" align="center" css={{ m: '10px 0 0 0' }}>
+            {isConnected && address ? (
               <>
                 <Button onClick={handler}>Get Certificate</Button>
               </>
             ) : (
               <>
                 {/* <Button disabled>Connect Wallet</Button> */}
-                <ConnectButton/>
+                {/* <ConnectButton /> */}
+                <Button disabled>Get Certificate</Button>
               </>
             )}
           </Row>
         </>
       )}
       <Spacer y={1} />
-      <Row justify="center" align="center" css={{ m: "10px 0 0 0" }}>
+      <Row justify="center" align="center" css={{ m: '10px 0 0 0' }}>
         <Text>Or</Text>
       </Row>
-      <Row justify="center" align="center" css={{ m: "10px 0 0 0" }}>
+      <Row justify="center" align="center" css={{ m: '10px 0 0 0' }}>
         <Text>
-          <Button onPress={() => router.push("/verify")}>
+          <Button onPress={() => router.push('/verify')}>
             Verify Certificate
           </Button>
         </Text>
@@ -261,7 +278,7 @@ export const GetCertificate = () => {
           {loading ? (
             <Loading
               type="points-opacity"
-              loadingCss={{ $$loadingSize: "50px" }}
+              loadingCss={{ $$loadingSize: '50px' }}
             />
           ) : (
             <>
@@ -280,7 +297,7 @@ export const GetCertificate = () => {
                 fullWidth
                 color="primary"
                 size="lg"
-                type="number" 
+                type="number"
                 min="100"
                 placeholder="Amount In Matic"
                 onChange={(e) => setMaticAmount(e.target.value)}
@@ -290,7 +307,7 @@ export const GetCertificate = () => {
         </Modal.Body>
         <Modal.Footer>
           {loading ? (
-            " "
+            ' '
           ) : (
             <>
               <Button auto flat color="error" onClick={closeHandler}>
